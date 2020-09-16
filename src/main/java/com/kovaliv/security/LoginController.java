6ff4a119b2faf3733a11cab3a940b2827cf88e8b
tkov1 <taraskovaliv@outlook.com>
2020-09-16 15:35:51 +0300

    Edit LoginController

--- src/main/java/com/kovaliv/security/LoginController.java
+++ src/main/java/com/kovaliv/security/LoginController.java
@@ -2,6 +2,7 @@ package com.kovaliv.security;
 
 import com.kovaliv.security.dtos.LoginDto;
 import com.kovaliv.security.dtos.LoginService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.ws.rs.GET;
@@ -10,6 +11,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Slf4j
 @Path("/login")
 @Produces(MediaType.APPLICATION_JSON)
 public class LoginController {
@@ -19,6 +21,7 @@ public class LoginController {
 
     @GET
     public Response get(LoginDto loginDto) {
+        log.info("Logged");
         return loginService.login(loginDto) ? Response.ok().build() : Response.status(Response.Status.FORBIDDEN).build();
     }
 }
