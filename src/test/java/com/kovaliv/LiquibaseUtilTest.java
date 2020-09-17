ccdddafa9b6e07dec1cb3cb6e7ce251f0ec44a4c
tkov1 <taraskovaliv@outlook.com>
2020-09-17 16:16:40 +0300

    Add LiquibaseUtilTest with test of update

---
+++ src/test/java/com/kovaliv/LiquibaseUtilTest.java
@@ -0,0 +1,14 @@
+package com.kovaliv;
+
+import com.kovaliv.config.LiquibaseUtil;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class LiquibaseUtilTest {
+
+    @Test
+    @Disabled
+    void testUpdate() {
+        LiquibaseUtil.update();
+    }
+}
