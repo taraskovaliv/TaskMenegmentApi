bafba5bb76402d0a5e6e9521f58ae89101197513
tkov1 <taraskovaliv@outlook.com>
2020-10-02 00:56:22 +0300

    Add modules in DateProvider

--- src/main/java/com/kovaliv/config/DateProvider.java
+++ src/main/java/com/kovaliv/config/DateProvider.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
 import javax.ws.rs.Produces;
@@ -19,7 +20,8 @@ public class DateProvider extends JacksonJaxbJsonProvider {
     static {
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.registerModule(new JodaModule()).setDateFormat(new StdDateFormat());
+        mapper.registerModule(new Jdk8Module());
+        mapper.registerModule(new JavaTimeModule()).setDateFormat(new StdDateFormat());
         mapper.findAndRegisterModules();
     }
 
